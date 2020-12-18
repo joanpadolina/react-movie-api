85bf702d02a74abfa644b49f068f87142df5aa44
Joan <Joanpadolina@gmail.com>
2020-12-18 14:42:12 +0100

    Added basic styling

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouteMatch } from "react-router-dom";
-
+import './movie.css'
 export default function Movie() {
   const [currentData, setCurrentData] = useState({});
   const match = useRouteMatch();
@@ -16,38 +16,46 @@ export default function Movie() {
       `https://api.themoviedb.org/3/movie/${movieParams}?api_key=${process.env.REACT_APP_ACCESS_TOKEN}&language=en-US`
     );
     const response = await data.json();
-    console.log(response)
+    console.log(response);
     return setCurrentData(response);
   }
 
   return (
-    <article className="movie-card">
-      <div className="movie-card__header">
+    <article className="movie">
+      <div className="movie__header">
         <img
-          className="movie-card__image"
+          className="movie__image"
           src={IMAGE_BASE_URL + currentData.poster_path}
           alt=""
           loading="lazy"
         ></img>
+        {currentData.backdrop_path && (
+          <img
+          className="movie__backdrop-image"
+            src={IMAGE_BASE_URL + currentData.backdrop_path}
+            alt=""
+            loading="lazy"
+          ></img>
+        )}
       </div>
-      <div className="movie-card__body-wrapper">
-        <h3 className="movie-card__title">{currentData.original_title}</h3>
-        <p className="movie-card__body">{currentData.overview}</p>
+      <div className="movie__body-wrapper">
+        <h3 className="movie__title title-big">{currentData.original_title}</h3>
+        <p className="movie__body body">{currentData.overview}</p>
       </div>
 
-      <div className="movie-card__footer">
-        <ul className="movie-card__genre">
+      <div className="movie__footer">
+        <ul className="movie__genre">
           {currentData &&
             currentData.genres?.map((item) => {
               return <li key={item.id}>{item.name}</li>;
             })}
         </ul>
 
-        <span className="movie-card__date">
+        <span className="movie__date">
           release date: {currentData.release_date}
         </span>
 
-        <span className="movie-card__vote-sum">
+        <span className="movie__vote-sum">
           {" "}
           avarage score: {currentData.vote_average}
         </span>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './navbar.css'
 export default function Navigation() {
     return(
         <nav>
             <ul>
-                <li><Link to="/"> home </Link></li>
+                <li className="navbar__item"><Link className="navbar__link" to="/"> AMA. <span className="navbar__subtitle"> another movie api </span> </Link></li>
             </ul>
         </nav>
     )
